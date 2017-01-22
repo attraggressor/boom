7735051bf1e381f7aa89268cfd2283bb9829b6f6
Dominik Spisak <dsperat@gmail.com>
2017-01-22 15:32:25 +0100

    first commit

--- elixir_chat/lib/elixir_chat.ex
+++ elixir_chat/lib/elixir_chat.ex
@@ -1,31 +1,58 @@
 defmodule ElixirChat do
 
+  # Gets user name and calls all primary actions
   def start do
+    # Get user name
     user = IO.gets("Type in your name: ") |> String.strip
+    # Display invitation using user name
     IO.puts "Hi #{user}, you just joined a chat room! Type your message in and press enter."
 
+    # Initalize AMQP and start connection
     {:ok, conn} = AMQP.Connection.open
+    # Create channel
     {:ok, channel} = AMQP.Channel.open(conn)
+    # Create queue
     {:ok, queue_data } = AMQP.Queue.declare channel, ""
 
+    # Declare a fanout exchange
     AMQP.Exchange.fanout(channel, "super.chat")
+    # Bind channell to the queue
     AMQP.Queue.bind channel, queue_data.queue, "super.chat"
 
+    # Start listening for messages
     listen_for_messages(channel, queue_data.queue)
     wait_for_message(user, channel)
   end
 
+  # Gets input from user and calls publish method then loops over using recursion
   def wait_for_message(user, channel) do
+    # Get user input
     message = IO.gets("") |> String.strip
+    # Publish message
     publish_message(user, message, channel)
+    # Call self
     wait_for_message(user, channel)
   end
 
+  # Intercepts messages and calls display method
   def listen_for_messages(channel, queue_name) do
+    # Go through all messages in the queue and display them
+    AMQP.Queue.subscribe channel, queue_name, fn(payload, _meta) ->
+      { :ok, data } = JSON.decode(payload)
+      display_message(data["user"], data["message"])
+    end
+  end
+  
+  # Displays a nicely formatted messages
+  def display_message(user, message) do
+    IO.puts "#{user}: #{message}"
   end
 
+  # Publishes messages to the queue
   def publish_message(user, message, channel) do
+    # Create ordered data to be easily processed
     { :ok, data } = JSON.encode([user: user, message: message])
+    # Publish message to the exchange channel
     AMQP.Basic.publish channel, "super.chat", "", data
   end
 
--- ruby_chat/chat.rb
+++ ruby_chat/chat.rb
@@ -2,43 +2,63 @@ require 'json'
 require 'bunny'
 class Chat
 
+  # Displays a nicely formatted messages
   def display_message(user, message)
     puts "#{user}: #{message}"
   end
 
+  # Class constructor where all primary actons are baing called
   def initialize
     print "Type in your name: "
+    # Get user name
     @current_user = gets.strip
+    # Display invitation using user name
     puts "Hi #{@current_user}, you just joined a chat room! Type your message in and press enter."
 
+    # Initalize Bunny and start connection
     conn = Bunny.new
     conn.start
 
+    # Create channel
     @channel = conn.create_channel
+
+    # Declare a fanout exchange
     @exchange = @channel.fanout("super.chat")
 
     listen_for_messages
   end
 
+  # Intercepts messages and calls display method
   def listen_for_messages
     queue = @channel.queue("")
 
+    # Go through all messages in the queue and display them
     queue.bind(@exchange).subscribe do |delivery_info, metadata, payload|
       data = JSON.parse(payload)
       display_message(data['user'], data['message'])
     end
   end
 
+  # Publishes messages to the queue
   def publish_message(user, message)
+    # Create ordered data to be easily processed
+    data = JSON.generate({ user: user, message: message })
+    # Publish message to the exchange channel
+    @exchange.publish(data)
   end
 
+  # Gets input from user and calls publish method then loops over using recursion
   def wait_for_message
+    # Get user input
     message = gets.strip
+    # Publish message
     publish_message(@current_user, message)
+    # Call self
     wait_for_message
   end
 
 end
 
 chat = Chat.new
+# Init user first message input
 chat.wait_for_message
